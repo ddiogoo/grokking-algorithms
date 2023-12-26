a3e3324fef690bbd8409b5f09d96027de47b70a0
Diogo Martins de Assis <74463348+diogomrtsa@users.noreply.github.com>
2023-12-26 08:23:34 -0300

    Criando código iterativo que busca chave dentro de caixas

---
+++ recursao.py
@@ -0,0 +1,11 @@
+## Pseudocódigo: a execução dos códigos abaixo não vai funcionar
+def procure_pela_chave(caixa_principal):
+    pilha = main_box.crie_uma_pilha_para_busca()
+    while pilha is not vazia:
+        caixa = pilha.pegue_uma_caixa()
+        for item in caixa:
+            if item.e_uma_caixa():
+                pilha.append(item)
+            elif item.e_uma_chave():
+                print("achei a chave!")
+                
